9f168dc96d5d23e348eaa10723431b6c412aed6a
Barry Mays <barrykmays@gmail.com>
2024-10-17 19:15:11 +0000

    Create initial views and basic html page

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"{self.title} | writen by {self.author}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -31,3 +31,9 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
     updated_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
