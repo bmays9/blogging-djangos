136ae8dcd348354c11dce36275d47b623e995c7c
Barry Mays <barrykmays@gmail.com>
2024-10-26 11:03:03 +0000

    Create About app, model and Urls wired up

---
+++ about/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
